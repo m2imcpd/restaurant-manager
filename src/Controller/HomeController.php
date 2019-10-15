e46f6f4a9f008c3c8524cd9da2887e0c702c296a
lionel-link <lioneladje@gmail.com>
2019-10-15 22:42:23 +0200

    update feat HomeController & view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,35 @@ class HomeController extends AbstractController
 {
 
     /**
+     * @var RestaurantRepository
+     */
+    private $repository;
+
+    /**
+     * Note: Injection du Repository Restaurant dans le constructeur pour résoudre la dépendance
+     * HomeController constructor.
+     * @param RestaurantRepository $repository
+     */
+    public  function __construct(RestaurantRepository $repository)
+    {
+
+        $this->repository = $repository;
+    }
+
+    /**
+     * Note: Retourne une liste d'objets de Restaurant accessibles par index
+     * grâce à la requête findAll du Repository Restaurant
      * @Route("/", name="home.index")
+     * @param RestaurantRepository $repository
      * @return Response
      */
-    public function index() : Response
+    public function index(RestaurantRepository $repository) : Response
     {
-        return new Response($this->renderView('home/index.html.twig'));
+        $restaurants = $this->repository->findAll();
+        dump($restaurants);
+        return $this->render('home/index.html.twig',[
+            'restaurants' =>$restaurants
+        ]);
     }
 
 }
