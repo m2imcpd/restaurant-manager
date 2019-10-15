99d298e180be54dca901550c2d92e63f4c785c76
lionel-link <lioneladje@gmail.com>
2019-10-15 23:46:52 +0200

    Add feat Create Admin\RestaurantController

--- src/Controller/Admin/RestaurantController.php
+++ src/Controller/Admin/RestaurantController.php
@@ -2,9 +2,11 @@
 namespace App\Controller\Admin;
 
 use App\Form\RestaurantType;
+use App\Entity\Restaurant;
 use App\Repository\RestaurantRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -45,4 +47,30 @@ class RestaurantController extends AbstractController
         return new Response($this->renderView('admin/restaurant/index.html.twig', compact('restaurants')));
     }
 
+    /**
+     * Note:Permet l'ajout d'un restaurant grâce à la génération d'un formulaire mappé sur l'entité restaurant
+     * avec une validation des donnéesau niveau du isValid et la persistance des données avec le object manager($em)
+     * suivie d'un message addflash passé a la vue
+     * @Route("/admin/restaurant/create", name= "admin.restaurant.new")
+     * @param Request $request permet de gérer la requête en l'occurence du POST
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function new(Request $request)
+    {
+        $restaurant = new Restaurant();
+        $form = $this->createForm(RestaurantType::class,$restaurant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($restaurant);
+            $this->em->flush();
+            $this->addFlash('succes', 'Créer avec succès');
+            return $this->redirectToRoute('admin.restaurant.index');
+        }
+
+        return $this->render('admin/restaurant/new.html.twig',[
+            'restaurant' => $restaurant,
+            'form' => $form->createView()
+        ]);
+    }
 }
