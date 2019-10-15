c29b1c7dce89b99fd1ffc9f32de6d4f9de92143b
lionel-link <lioneladje@gmail.com>
2019-10-16 00:16:07 +0200

    Add build Form HoraireType Horaire Entity

---
+++ src/Form/HoraireType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Horaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HoraireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('day')
+            ->add('open_hour_at')
+            ->add('close_hour_at')
+            ->add('restaurant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Horaire::class,
+        ]);
+    }
+}
